c669b95c9289a85f918285eddada2fc5d64fd7a1
airportyh <airportyh@gmail.com>
2011-08-01 22:35:15 -0400

    Got tabs "first draft" working.

--- cmd.js
+++ cmd.js
@@ -9,12 +9,15 @@ var Server = require('server').Server,
 function AppView(app){
     this.app = app
     this.curses = require('ncurses')
-    this.win = new this.curses.Window()
     this.curses.showCursor = false
+    this.win = new this.curses.Window()
+    this.setupColorPairs()
+    this._currentTab = -1
     this.win.on('inputChar', this.onInputChar.bind(this))
     process.on('uncaughtException', function(err){
         this.win.close()
         console.log(err.stack)
+        process.exit()
     }.bind(this))
     process.on('SIGINT', function(err){
         this.win.close()
@@ -23,46 +26,135 @@ function AppView(app){
     this.cbs = []
 }
 AppView.prototype = {
+    setupColorPairs: function(){
+        var idx = 0
+        this.curses.colorPair(idx, this.curses.colors.WHITE, this.curses.colors.BLACK)
+        this.NORMAL = this.curses.colorPair(idx++)
+        this.curses.colorPair(idx, this.curses.colors.RED, this.curses.colors.BLACK)
+        this.CURRENT_TAB = this.curses.colorPair(idx++)
+    },
+    failedBrowsers: function(){
+        return this.app.server.browsers.filter(function(b){
+            return b.results && b.results.failed > 0
+        })
+    },
+    currentTab: function(){
+        if (this._currentTab === -1)
+            if (this.app.server.browsers.length > 0)
+                if (this.failedBrowsers().length > 0)
+                    this._currentTab = 0
+        else if (this.failedBrowsers().length === 0)
+            this._currentTab = -1
+        return this._currentTab
+    },
+    colWidth: function(){
+        return 12
+    },
+    pad: function(str, l, s, t){
+        return s || (s = " "), (l -= str.length) > 0 ? (s = new Array(Math.ceil(l / s.length)
+            + 1).join(s)).substr(0, t = !t ? l : t == 1 ? 0 : Math.ceil(l / 2))
+            + str + s.substr(0, l - t) : str;
+    },
     on: function(event, cb){
         if (event === 'inputChar')
             this.cbs.push(cb)
     },
     onInputChar: function(chr, i){
-        this.cbs.forEach(function(cb){
-            cb(chr, i)
-        })
+        try{
+            if (i === 261) // right arrow
+                this.nextTab()
+            else if (i === 260) // left arrow
+                this.prevTab()
+            this.cbs.forEach(function(cb){
+                cb(chr, i)
+            })
+        }catch(e){
+            this.app.log.error('In onInputChar: ' + e)
+        }
+    },
+    nextTab: function(){
+        if (this.currentTab() >= 0){
+            this._currentTab++
+            var fb = this.failedBrowsers()
+            if (this._currentTab >= fb.length)
+              this._currentTab = 0
+        }
+        this.renderBrowserHeaders()
+        this.renderLogPanel()
+    },
+    prevTab: function(){
+        if (this.currentTab() >= 0){
+            this._currentTab--
+            var fb = this.failedBrowsers()
+            if (this._currentTab < 0)
+                this._currentTab = fb.length - 1
+        }
+        this.renderBrowserHeaders()
+        this.renderLogPanel()
     },
     renderTitle: function(){
-        this.win.addstr(0, 0, "LET\u0027S TEST\u0027EM \u0027SCRIPTS!")
+        this.writeLine(0, "LET\u0027S TEST\u0027EM \u0027SCRIPTS!")
     },
     renderTopInstructions: function(){
-        this.win.addstr(1, 0, 'Open http://' + this.app.server.ipaddr + ':' + 
-            this.app.server.config.port + '/ in a browser to connect.')
+        this.writeLine(1, 'Open the URL below in a browser to connect.')
+        this.writeLine(2, 'http://' + this.app.server.ipaddr + ':' + 
+            this.app.server.config.port + '/')
+    },
+    writeLine: function(row, str){
+        this.win.addstr(row, 0, this.pad(str, this.curses.cols, ' ', 1))
     },
     renderBrowserHeaders: function(){
-        this.app.log.info('num browsers: ' + this.app.server.browsers.length)
-        var text = this.app.server.browsers.map(function(browser){
-            return browser.name
-        }).join('  ')
-        this.app.log.info('text: ' + text)
-        this.win.addstr(3, 0, text)
+        this.app.server.browsers.forEach(function(browser, idx){
+            if (this.currentTab() === idx)
+                this.win.attrset(this.CURRENT_TAB)
+            this.win.addstr(4, this.colWidth() * idx,
+                this.pad(browser.name || '', this.colWidth(), ' ', 2))
+                if (this.currentTab() === idx)
+                    this.win.attrset(this.NORMAL)
+        }.bind(this))
     },
     renderTestResults: function(){
         var text = this.app.server.browsers.map(function(b){
+            var ret
             if (b.results)
-                return b.results.passed + '/' + b.results.total
+                ret = b.results.passed + '/' + b.results.total
             else
-                return ''
-        }).join('  ')
-        this.win.addstr(4, 0, text)
+                ret = ''
+            return this.pad(ret, this.colWidth(), ' ', 2)
+        }.bind(this)).join('  ')
+        
+        this.writeLine(5, text)
+        
+    },
+    bottomInstructions: function(){
+        if (this.app.server.browsers.length === 0)
+            return '[q to quit]'
+        else
+            return '[Press ENTER to run tests; q to quit]'
+    },
+    stashCursor: function(){
+        this.win.cursor(this.curses.lines - 1, this.bottomInstructions().length)
     },
     renderBottomInstructions: function(){
-        var text
-        if (this.app.server.browsers.length === 0)
-            text = '[q to quit]'
-        else
-            text = '[Press Enter to run tests; q to quit]'
-        this.win.addstr(this.curses.lines - 1, 0, text)
+        this.win.addstr(this.curses.lines - 1, 0, this.bottomInstructions())
+    },
+    print: function(win, ln, str){
+        str.split('\n').forEach(function(line){
+            this.writeLine(ln++, line)
+        }.bind(this))
+    },
+    renderLogPanel: function(){
+        var browser = this.app.server.browsers[this.currentTab()]
+        if (browser)
+            this.app.log.info('error msg for ' + browser.name)
+        if (!browser || !browser.results) return
+        if (browser.results.items){
+            browser.results.items.forEach(function(item){
+                var out = item.name + '\n    ' + 
+                    item.message
+                this.print(this.win, 7, out)
+            }.bind(this))
+        }
     },
     renderAll: function(){
         this.renderTitle()
@@ -73,6 +165,7 @@ AppView.prototype = {
     },
     refresh: function(){
         setTimeout(function(){
+            this.stashCursor()
             this.win.refresh()
         }.bind(this), 1)
     },
@@ -116,11 +209,15 @@ App.prototype = {
         this.view.refresh()
     },
     onTestResult: function(){
+        this.view.renderBrowserHeaders()
         this.view.renderTestResults()
+        this.view.renderLogPanel()
         this.view.refresh()
     },
     onAllTestResults: function(){
+        this.view.renderBrowserHeaders()
         this.view.renderTestResults()
+        this.view.renderLogPanel()
         this.view.refresh()
     }
 }
