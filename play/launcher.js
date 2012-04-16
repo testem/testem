100b7abcd491586954b1a4eff06ce2bd74f91035
airportyh <airportyh@gmail.com>
2012-04-15 22:14:08 -0400

    Started on OSX.

--- play/launcher.js
+++ play/launcher.js
@@ -1,15 +1,22 @@
 var spawn = require('child_process').spawn
 
+//var browsers = [
+//    ["IE", "C:\\Program Files\\Internet Explorer\\iexplore.exe"],
+//    ["Chrome", "C:\\Users\\airportyh\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe", ["--start-maximized"]],
+//    ["Firefox", "C:\\Program Files\\Mozilla Firefox\\firefox.exe"],
+//    ["Safari", "C:\\Program Files\\Safari\\safari.exe"]
+//]
+
 var browsers = [
-    ["IE", "C:\\Program Files\\Internet Explorer\\iexplore.exe"],
-    ["Chrome", "C:\\Users\\airportyh\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe", ["--start-maximized"]],
-    ["Firefox", "C:\\Program Files\\Mozilla Firefox\\firefox.exe"],
-    ["Safari", "C:\\Program Files\\Safari\\safari.exe"]
+  ["PhantomJS", "/usr/local/bin/phantomjs"],
+  ["Chrome", "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome", ["--user-data-dir=/tmp/chrome", "--no-default-browser-check", "--no-first-run"]],
+  ["Safari", "/Applications/Safari.app/Contents/MacOS/Safari"],
+  ["Firefox", "/Applications/Firefox.app/Contents/MacOS/firefox"]
 ]
 
 var left = browsers.slice(0)
 var url = "http://tobyho.com"
-function launchOne(){
+function launchNext(){
     var browser = left.shift()
     if (!browser) return
     var name = browser[0]
@@ -19,8 +26,8 @@ function launchOne(){
     var p = spawn(exe, args)
     setTimeout(function(){
         p.kill("SIGTERM")
-        setTimeout(launchOne, 1000)
+        setTimeout(launchNext, 1000)
     }, 5000)
 }
-launchOne()
+launchNext()
 
