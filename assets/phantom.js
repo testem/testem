b95d3bd2dcbd851179f01e58aebd98c224d94f20
airportyh <airportyh@gmail.com>
2012-04-23 22:19:09 -0400

    Fixed phantomJS not launching in dev mode.

--- assets/phantom.js
+++ assets/phantom.js
@@ -1,9 +1,3 @@
 var page = new WebPage()
-page.onConsoleMessage = function(msg) {
-    console.log(msg)
-}
-page.open('http://localhost:3580/', function(status){
-    page.evaluate(function(){
-        console.log(navigator.userAgent)
-    })
-})
+var port = phantom.args[0]
+page.open('http://localhost:' + port + '/')
