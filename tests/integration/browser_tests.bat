0fd5085d369699f53ef82f8f05438662c18d37c7
Toby Ho <airportyh@gmail.com>
2012-05-11 00:21:13 -0400

    Better bat script

--- tests/integration/browser_tests.bat
+++ tests/integration/browser_tests.bat
@@ -1,13 +1,15 @@
 @echo off
-REM FOR %%dir IN (jasmine_custom jasmine_requirejs jasmine_simple qunit_simple) DO ECHO %%dir
+node testem.js ci -l
 for /f %%a in ('dir /ad /b examples') do call :testem %%a
 
 exit /b
 
 rem test'em for one example
 :testem
-echo "Testing %1..."
+echo Testing %1...
 cd examples\%1
-node ..\..\testem.js ci
+node ..\..\testem.js ci > out.tap
+type out.tap | findstr /R /C:"# tests [0-9]*" /C:"# pass  [0-9]*"
+del out.tap
 cd ..\..
 exit /b
