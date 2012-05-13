a919cd0056de89b77a57855e1c6d2a65087adc2f
airportyh <airportyh@gmail.com>
2012-05-12 23:10:46 -0400

    Auto-select the first browser.

--- examples/jasmine_custom/hello_spec.js
+++ examples/jasmine_custom/hello_spec.js
@@ -2,4 +2,7 @@ describe('hello', function(){
     it('should say hello', function(){
         expect(hello()).toBe('hello world')
     })
+    it('should die', function(){
+    	throw new Error('die')
+    })
 })
