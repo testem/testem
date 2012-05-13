d0038c2fb32c3a585cb8b0181248f22c3cedbbb5
airportyh <airportyh@gmail.com>
2012-05-12 22:07:48 -0400

    New design for tabs.

--- examples/long_running/tests.js
+++ examples/long_running/tests.js
@@ -2,7 +2,7 @@ describe('setTimeout', function(){
 	for (var i = 0; i < 5; i++){
 		it('should wait for some time (' + (i+1) + ')', function(){
 			waits(400)
-			expect(true).toBe(true)
+			expect(true).toBe(false)
 		})
 	}
 })
