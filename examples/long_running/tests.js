448b37c4179eb6987a10ce9b3ca9f34b9ac436ea
airportyh <airportyh@gmail.com>
2012-05-22 23:36:32 -0400

    Got basic log panel scrolling working.

--- examples/long_running/tests.js
+++ examples/long_running/tests.js
@@ -2,6 +2,7 @@ describe('setTimeout', function(){
 	for (var i = 0; i < 5; i++){
 		it('should wait for some time (' + (i+1) + ')', function(){
 			waits(400)
+			throw new Error('blah')
 		})
 	}
 })
