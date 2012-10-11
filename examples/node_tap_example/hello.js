55a46ebc5152d23e9993c7fd146ea59e60998271
airportyh <airportyh@gmail.com>
2012-10-10 23:01:24 -0400

    Completed node-tap example.

---
+++ examples/node_tap_example/hello.js
@@ -0,0 +1,5 @@
+function hello(){
+    return 'hello world';
+}
+
+module.exports = hello
--- examples/node_tap_example/tests.js
+++ examples/node_tap_example/tests.js
@@ -2,6 +2,6 @@ var test = require('tap').test;
 var hello = require('./hello');
 
 test('it says hello?', function(t){
-    t.equal(hello(), 'hello');
+    t.equal(hello(), 'hello world');
     t.end();
 });
