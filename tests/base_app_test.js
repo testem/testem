cc1412b7c2d7ade148a7d6a1dd66f86bc4c29038
Thomas Rix <tom@rixth.org>
2012-10-19 23:04:53 -0700

    Clean up test code

--- tests/base_app_test.js
+++ tests/base_app_test.js
@@ -1,24 +1,13 @@
 var BaseApp = require('../lib/base_app')
-var test = require('./testutils.js')
+var expect = require('./testutils.js').expect
 var spy = require('sinon').spy
 var Model = require('backbone').Model
-var stub = require('sinon').stub
-var EventEmitter = require('events').EventEmitter
-var expect = test.expect
 
 describe('BaseApp', function(){
-    var socket, app, runner1, runner2, server
+    var app, runner1, runner2
 
     beforeEach(function(){
-        socket = new EventEmitter
-        server = {
-            emit: test.spy()
-            , cleanUpConnections: test.spy()
-            , removeBrowser: test.spy()
-        }
-
         app = new BaseApp(new Model({ port: 3000 }))
-        app.server = server
 
         runner1 = new Model({ results: new Model({ all: false }) })
         runner2 = new Model({ results: new Model({ all: false }) })
@@ -27,7 +16,7 @@ describe('BaseApp', function(){
     })
 
     it('runs the postprocessors once all runners have reported all test results', function(){
-        app.runPostprocessors = test.spy()
+        app.runPostprocessors = spy()
 
         runner1.get('results').set('all', true)
         app.emit('all-test-results')
@@ -39,7 +28,7 @@ describe('BaseApp', function(){
     })
 
     it('runs the postprocessors at exit', function(){
-        app.runExitHook = test.spy()
+        app.runExitHook = spy()
 
         app.emit('exit')
         expect(app.runExitHook.called).to.be.ok
