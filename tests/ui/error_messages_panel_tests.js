e9856501379b1847e0bb7ab784fe61c0010efbb9
airportyh <airportyh@gmail.com>
2012-10-23 22:25:15 -0400

    One test in error_messages_panel.js

--- tests/ui/error_messages_panel_tests.js
+++ tests/ui/error_messages_panel_tests.js
@@ -1,5 +1,29 @@
+var libDir = '../../lib/'
+var sandbox = require('sandboxed-module')
+var screen = require('./fake_screen')
+var ScrollableTextPanel = sandbox.require(libDir + 'ui/scrollable_text_panel', {
+    requires: {
+        './screen': screen
+    }
+})
+var ErrorMessagesPanel = sandbox.require(libDir + 'ui/error_messages_panel', {
+    requires: {
+        './screen': screen
+        , './scrollable_text_panel': ScrollableTextPanel
+    }
+})
+
 describe('ErrorMessagesPanel', function(){
-    it('does stuff', function(){
-        
+    var panel
+    beforeEach(function(){
+        panel = new ErrorMessagesPanel({
+            line: 0
+            , col: 0
+            , width: 10
+            , height: 10
+            , text: 'blah'
+        })
     })
+    it('initializes', function(){})
+
 })
