75dc618df21314255214b4ec02b567f2e5f88e87
airportyh <airportyh@gmail.com>
2012-11-13 11:37:49 -0500

    Started work on merging the template branch and started writing tests for server.js.

--- tests/server_tests.js
+++ tests/server_tests.js
@@ -1,16 +1,76 @@
-var Server = require('../lib/server.js')
+var Server = require('../lib/server')
+var Config = require('../lib/config')
 var test = require('./testutils.js')
 var EventEmitter = require('events').EventEmitter
 var Backbone = require('backbone')
+var request = require('request')
+var jsdom = require('jsdom')
+var fs = require('fs')
+
 var expect = test.expect
 
-describe('Server', function(){
-	var server, runners, app, socketClient
-	var orgSetTimeout
-	beforeEach(function(){
+describe.only('Server', function(){
+	var server, runners, app, socketClient, config
+	var orgSetTimeout, baseUrl, port
+	before(function(done){
+        port = 73571
+        config = new Config('dev', {
+            port: port,
+            src_files: [
+                'data/hello.js',
+                'data/hello_tests.js'
+            ]
+        })
+        baseUrl = 'http://localhost:' + port + '/'
 		runners = new Backbone.Collection
-		app = {config: {}, runners: runners, removeBrowser: function(){}}
+
+		app = {config: config, runners: runners, removeBrowser: function(){}}
 		server = new Server(app)
+        server.start()
+        server.once('server-start', function(){
+            done()
+        })
 		socketClient = new EventEmitter
 	})
+    after(function(done){
+        server.stop(function(){
+            done()
+        })
+    })
+
+    it('gets home page', function(done){
+        var port = config.get('port')
+        jsdom.env(baseUrl, function(err, window){
+            var document = window.document
+            expect(window.document.title).to.equal('Test\'em Runner')
+            var scripts = document.getElementsByTagName('script')
+            var srcs = Array.prototype.map.call(scripts, function(s){ return s.src })
+            expect(srcs).to.deep.equal([ 
+                '/testem/jasmine.js',
+                '/testem.js',
+                '/testem/jasmine-html.js',
+                '',
+                'data/hello.js',
+                'data/hello_tests.js' ])
+            done()
+        })
+    })
+
+    function assertUrlReturnsFileContents(url, file, done){
+        request(url, function(err, req, text){
+            expect(text).to.equal(fs.readFileSync(file).toString())
+            done()
+        })
+    }
+
+    it('gets src file', function(done){
+        assertUrlReturnsFileContents(baseUrl + 'data/hello.js', 'data/hello.js', done)
+    })
+
+    it('gets bundled files', function(done){
+        assertUrlReturnsFileContents(baseUrl + 'testem/jasmine.js', '../public/testem/jasmine.js', done)
+    })
+
+
+    
 })
