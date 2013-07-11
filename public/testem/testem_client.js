4d8fb0a63671f901e3c80a65cf1d2e95d86dfbea
Blake Owens <owensbla@gmail.com>
2013-07-11 12:33:36 -0700

    Pass through the socket.io connection so the socket connection establishes with the correct server in the rare case that we are pulling in testem.js from a separate port.

--- public/testem/testem_client.js
+++ public/testem/testem_client.js
@@ -183,7 +183,7 @@ function getId(){
 function init(){
     takeOverConsole()
     interceptWindowOnError()
-    socket = io.connect()
+    socket = io.connect(socketConnectionPath)
     var id = getId()
     socket.emit('browser-login', 
         getBrowserName(navigator.userAgent), 
