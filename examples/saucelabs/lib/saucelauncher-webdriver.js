abf726bdc9e6b93405fc6ba967adf479d91d76be
Igor Lima <igor.lima@evolut.io>
2013-08-06 21:50:31 -0300

    Create a file which will connect and launcher

---
+++ examples/saucelabs/lib/saucelauncher-webdriver.js
@@ -0,0 +1,19 @@
+var launcher = require('sauce-connect-launcher'),
+    config   = require('./sauce-conf.js'),
+    integrationTest = require('./sauce-javascript-tests-integration.js');
+
+launcher(config.launcherOptions, function (err, sauceConnectProcess) {
+  console.log("Started Sauce Connect Process");
+
+  integrationTest(
+    "http://localhost:7357",
+    "{failedCount: jasmine.currentEnv_.currentRunner_.results().failedCount}",
+
+    function() {
+      sauceConnectProcess.close(function () {
+        console.log("Closed Sauce Connect process");
+      });
+    }
+
+  );
+});
