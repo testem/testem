5e8c2ff637e7958e68d17b415ed0343e4e3d3ba9
airportyh <airportyh@gmail.com>
2013-08-19 14:22:22 -0400

    Refactored the changes in `port_in_use`.

--- tests/integration/browser_tests.sh
+++ tests/integration/browser_tests.sh
@@ -2,9 +2,17 @@
 cd examples
 for dir in $(ls)
 do
+  if [ $dir == "browserstack" ]
+  then
+    continue
+  fi
+  if [ $dir == "saucelabs" ]
+  then
+    continue
+  fi
 	echo "Testing $dir..."
-	cd $dir
-	../../testem.js ci | pcregrep -M "# tests ([0-9]+)\n# (pass|fail)  ([0-9]+)"
+  cd $dir
+	../../testem.js ci -P 10 | pcregrep -M "# tests ([0-9]+)\n# (pass|fail)  ([0-9]+)"
 	cd ..
 done
 cd ..
