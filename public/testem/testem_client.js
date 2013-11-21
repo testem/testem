eaf770d375f5d66617b0b93712f5a521031d7a50
airportyh <airportyh@gmail.com>
2013-11-20 22:20:30 -0500

    Updated Opera agent detection (#315).

--- public/testem/testem_client.js
+++ public/testem/testem_client.js
@@ -81,9 +81,12 @@ function patchSocketIOReconnect(){
 function getBrowserName(userAgent){
     var regexs = [
         /MS(?:(IE) (1?[0-9]\.[0-9]))/,
+        [/(OPR)\/([0-9]+\.[0-9]+)/, function(m){
+            return ['Opera', m[2]].join(' ')
+        }],
+        /(Opera).*Version\/([0-9]+\.[0-9]+)/,
         /(Chrome)\/([0-9]+\.[0-9]+)/,
         /(Firefox)\/([0-9a-z]+\.[0-9a-z]+)/,
-        /(Opera).*Version\/([0-9]+\.[0-9]+)/,
         /(PhantomJS)\/([0-9]+\.[0-9]+)/,
         [/(Android).*Version\/([0-9]+\.[0-9]+).*(Safari)/, function(m){
             return [m[1], m[3], m[2]].join(' ')
