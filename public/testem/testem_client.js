13951c27dde11dbff3be718fe7641c73c28bbfca
johanneswuerbach <johannes.wuerbach@googlemail.com>
2015-01-05 14:30:44 +0100

    Upgraded socket.io and express

--- public/testem/testem_client.js
+++ public/testem/testem_client.js
@@ -9,75 +9,6 @@ It also restarts the tests by refreshing the page when instructed by the server 
 */
 
 
-patchSocketIOReconnect()
-// patch Socket.IO so it doesn't use the exponential delay algorithm for
-// reconnection so that if you quit testem and then restart again, the
-// reconnection will be quick.
-function patchSocketIOReconnect(){
-
-  io.Socket.prototype.reconnect = function () {
-    this.reconnecting = true;
-    this.reconnectionAttempts = 0;
-    this.reconnectionDelay = this.options['reconnection delay'];
-
-    var self = this
-      , tryMultiple = this.options['try multiple transports']
-      , limit = this.options['reconnection limit'];
-
-    function reset () {
-      if (self.connected) {
-        for (var i in self.namespaces) {
-          if (self.namespaces.hasOwnProperty(i) && '' !== i) {
-              self.namespaces[i].packet({ type: 'connect' });
-          }
-        }
-        self.publish('reconnect', self.transport.name, self.reconnectionAttempts);
-      }
-
-      clearTimeout(self.reconnectionTimer);
-
-      self.removeListener('connect_failed', maybeReconnect);
-      self.removeListener('connect', maybeReconnect);
-
-      self.reconnecting = false;
-
-      delete self.reconnectionAttempts;
-      delete self.reconnectionDelay;
-      delete self.reconnectionTimer;
-      delete self.redoTransports;
-
-      self.options['try multiple transports'] = tryMultiple;
-    };
-
-    function maybeReconnect () {
-      if (!self.reconnecting) {
-        return;
-      }
-
-      if (self.connected) {
-        return reset();
-      };
-
-      if (self.connecting && self.reconnecting) {
-        return self.reconnectionTimer = setTimeout(maybeReconnect, 1000);
-      }
-
-
-      self.connect();
-      self.publish('reconnecting', self.reconnectionDelay, self.reconnectionAttempts);
-      self.reconnectionTimer = setTimeout(maybeReconnect, self.reconnectionDelay);
-
-    };
-
-    this.options['try multiple transports'] = false;
-    this.reconnectionTimer = setTimeout(maybeReconnect, this.reconnectionDelay);
-
-    this.on('connect', maybeReconnect);
-  };
-
-}
-
-
 function getBrowserName(userAgent){
   var regexs = [
     /MS(?:(IE) (1?[0-9]\.[0-9]))/,
@@ -186,10 +117,10 @@ function getId(){
 function init(){
   takeOverConsole()
   interceptWindowOnError()
-  socket = io.connect()
+  socket = io.connect({ reconnectionDelayMax: 1000 })
   var id = getId()
-  socket.emit('browser-login', 
-    getBrowserName(navigator.userAgent), 
+  socket.emit('browser-login',
+    getBrowserName(navigator.userAgent),
     id)
   socket.on('connect', function(){
     connectStatus = 'connected'
@@ -299,11 +230,11 @@ function getPath (value, seen, keys) {
 }
 
 function stringify(obj, fn, spaces, decycle) {
-  return JSON.stringify(obj, getSerialize(fn, decycle), spaces);
+  return xJSON.stringify(obj, getSerialize(fn, decycle), spaces);
 }
 
 function circularSafe(obj){
-  return JSON.parse(stringify(obj))
+  return xJSON.parse(stringify(obj))
 }
 
 function emit(){
@@ -338,6 +269,7 @@ window.Testem = {
   handleConsoleMessage: function(){}
 }
 
-var JSON = window.JSON || JSON2()
+var localJSON3 = JSON3.noConflict()
+var xJSON = window.JSON || localJSON3
 
 init()
