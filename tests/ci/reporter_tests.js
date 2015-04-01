0f9769288979a0cd60c7388870f0d67225ddf43e
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-04-01 10:07:30 +0100

    Added test for escaping messages

--- tests/ci/reporter_tests.js
+++ tests/ci/reporter_tests.js
@@ -99,9 +99,9 @@ describe('test reporters', function(){
       var reporter = new XUnitReporter(false, stream)
       reporter.report('phantomjs', {
         name: 'it didnt work',
-        passed: 1,
+        passed: 0,
         total: 1,
-        failed: 0,
+        failed: 1,
         error: new Error('it crapped out')
       })
       reporter.finish()
@@ -114,15 +114,30 @@ describe('test reporters', function(){
       var reporter = new XUnitReporter(false, stream)
       reporter.report('phantomjs', {
         name: 'it failed with quotes',
-        passed: 1,
+        passed: 0,
         total: 1,
-        failed: 0,
+        failed: 1,
         error: new Error('<it> \"crapped\" out')
       })
       reporter.finish()
       var output = stream.read().toString()
       assert.match(output, /it failed with quotes"><failure/)
       assert.match(output, /&lt;it&gt; &quot;crapped&quot; out/)
+    })
+    it('XML escapes messages', function(){
+      var stream = new PassThrough()
+      var reporter = new XUnitReporter(false, stream)
+      reporter.report('phantomjs', {
+        name: 'it failed with ampersands',
+        passed: 0,
+        total: 1,
+        failed: 1,
+        error: { message: "managers/slide-preview.js should pass jshint.managers/slide-preview.js: line 34, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 51, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 99, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 106, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 113, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 122, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 146, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 156, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 160, col 17, Bad line breaking before '&&'.managers/slide-preview.js: line 234, col 47, Expected a 'break' statement before 'default'.managers/slide-preview.js: line 264, col 68, Expected an assignment or function call and instead saw an expression.managers/slide-preview.js: line 209, col 13, 'slide' is defined but never used.managers/slide-preview.js: line 256, col 13, 'remainingPreviews' is defined but never used.13 errors" }
+      })
+      reporter.finish()
+      var output = stream.read().toString()
+      assert.match(output, /it failed with ampersands"><failure/)
+      assert.match(output, /&amp;&amp;/)
     })
   })
 
