f66e69c006141ee546814123310c7670d5e13771
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-04-01 11:44:32 +0100

    Switch to using xml dom rather than string concat for guaranteed parseable xml

--- tests/ci/reporter_tests.js
+++ tests/ci/reporter_tests.js
@@ -2,6 +2,7 @@ var TapReporter = require('../../lib/ci/test_reporters/tap_reporter')
 var DotReporter = require('../../lib/ci/test_reporters/dot_reporter')
 var XUnitReporter = require('../../lib/ci/test_reporters/xunit_reporter')
 var PassThrough = require('stream').PassThrough
+var XmlDom = require('xmldom')
 var assert = require('chai').assert
 
 describe('test reporters', function(){
@@ -60,6 +61,8 @@ describe('test reporters', function(){
       var output = stream.read().toString()
       assert.match(output, /  \./)
       assert.match(output, /1 tests complete \([0-9]+ ms\)/)
+
+      assertXmlIsValid(output)
     })
 
     it('writes out errors', function(){
@@ -92,7 +95,9 @@ describe('test reporters', function(){
       reporter.finish()
       var output = stream.read().toString()
       assert.match(output, /<testsuite name="Testem Tests" tests="1" failures="0" timestamp="(.+)" time="([0-9]+)">/)
-      assert.match(output, /<testcase name="phantomjs it does &lt;cool&gt; &quot;cool&quot; &apos;cool&apos; stuff"\/>/)
+      assert.match(output, /<testcase classname="phantomjs" name="it does &lt;cool> &quot;cool&quot; \'cool\' stuff"/)
+
+      assertXmlIsValid(output)
     })
     it('outputs errors', function(){
       var stream = new PassThrough()
@@ -106,8 +111,10 @@ describe('test reporters', function(){
       })
       reporter.finish()
       var output = stream.read().toString()
-      assert.match(output, /it didnt work"><failure/)
+      assert.match(output, /it didnt work"/)
       assert.match(output, /it crapped out/)
+
+      assertXmlIsValid(output)
     })
     it('XML escapes errors', function(){
       var stream = new PassThrough()
@@ -121,8 +128,10 @@ describe('test reporters', function(){
       })
       reporter.finish()
       var output = stream.read().toString()
-      assert.match(output, /it failed with quotes"><failure/)
-      assert.match(output, /&lt;it&gt; &quot;crapped&quot; out/)
+      assert.match(output, /it failed with quotes"/)
+      assert.match(output, /&lt;it> &quot;crapped&quot; out/)
+
+      assertXmlIsValid(output)
     })
     it('XML escapes messages', function(){
       var stream = new PassThrough()
@@ -132,13 +141,50 @@ describe('test reporters', function(){
         passed: 0,
         total: 1,
         failed: 1,
-        error: { message: "managers/slide-preview.js should pass jshint.managers/slide-preview.js: line 34, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 51, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 99, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 106, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 113, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 122, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 146, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 156, col 13, Expected '{' and instead saw 'return'.managers/slide-preview.js: line 160, col 17, Bad line breaking before '&&'.managers/slide-preview.js: line 234, col 47, Expected a 'break' statement before 'default'.managers/slide-preview.js: line 264, col 68, Expected an assignment or function call and instead saw an expression.managers/slide-preview.js: line 209, col 13, 'slide' is defined but never used.managers/slide-preview.js: line 256, col 13, 'remainingPreviews' is defined but never used.13 errors" }
+        error: { message: "&&" }
       })
       reporter.finish()
       var output = stream.read().toString()
-      assert.match(output, /it failed with ampersands"><failure/)
+      assert.match(output, /it failed with ampersands"/)
       assert.match(output, /&amp;&amp;/)
+
+      assertXmlIsValid(output)
+    })
+    it('presents valid XML with null messages', function(){
+      var stream = new PassThrough()
+      var reporter = new XUnitReporter(false, stream)
+      reporter.report('phantomjs', {
+        name: 'null',
+        passed: 0,
+        total: 1,
+        failed: 1,
+        error: { message: null }
+      })
+      reporter.finish()
+      var output = stream.read().toString()
+
+      assertXmlIsValid(output)
     })
   })
+
+var assertXmlIsValid = function(xmlString) {
+  var failure = null;
+  var parser = new XmlDom.DOMParser({
+    errorHandler:{
+      locator:{},
+      warning: function(txt) { failure = txt; },
+      error: function(txt) { failure = txt; },
+      fatalError: function(txt) { failure = txt; }
+    }
+  })
+
+  // this will throw into failure variable with invalid xml
+  parser.parseFromString(xmlString,'text/xml')
+
+  if (failure)
+  {
+    assert(false, failure+'\n---\n'+xmlString+'\n---\n')
+  }
+}
 
 })
