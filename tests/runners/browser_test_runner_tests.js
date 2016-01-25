testem/testem

commit abb0fdcf2c6c84405999b632c9eda8a34076852e

Fixes browser runner error handling
Return correct exit code on global errors, correctly set missing
launcherId on exit and disconnect.

lib/runners/browser_test_runner.js
@@ -32,45 +32,26 @@ BrowserTestRunner.prototype = {
 
     log.info('tryAttach', browser, id);
 
-    var self = this;
     if (this.pending) {
       clearTimeout(this.pending);
     }
+
     this.pending = false;
     this.socket = socket;
-
-    var runner = {
-      browserName: browser,
+    this.browser = browser;
+    this.runner = {
+      browserName: this.browser,
       reporter: this.reporter,
       logs: [],
       launcherId: this.launcher.id
     };
 
-    this.onStart.call(runner);
+    this.onStart.call(this.runner);
 
-    var config = this.launcher.config;
+    socket.on('test-result', this.onTestResult.bind(this.runner));
+    socket.on('top-level-error', this.onGlobalError.bind(this));
 
-    socket.on('test-result', this.onTestResult.bind(runner));
-
-    socket.on('top-level-error', function(msg, url, line) {
-      var message = msg + ' at ' + url + ', line ' + line + '\n';
-      runner.logs.push({
-        type: 'error',
-        text: message
-      });
-
-      if (config.get('bail_on_uncaught_error')) {
-        self.onTestResult.call(runner, {
-          passed: false,
-          name: 'Global error: ' + msg + ' at ' + url + ', line ' + line + '\n',
-          logs: [],
-          error: {}
-        });
-        self.onAllTestResults();
-        self.onEnd.call(runner);
-      }
-    });
-
+    var runner = this.runner;
     var handleMessage = function(type) {
       return function(/* ...args */) {
         var args = Array.prototype.slice.call(arguments);
@@ -90,26 +71,13 @@ BrowserTestRunner.prototype = {
     socket.on('warn', handleMessage('warn'));
     socket.on('log', handleMessage('log'));
 
-    socket.on('disconnect', function() {
-      browser.pending = setTimeout(function() {
-        if (self.finished) {
-          return;
-        }
-
-        var result = {
-          passed: false,
-          name: 'Browser ' + self.launcher.commandLine() + ' disconnected unexpectedly.'
-        };
-        self.reporter.report(self.name(), result);
-        self.finish();
-      }, 1000);
-    });
+    socket.on('disconnect', this.onDisconnect.bind(this));
 
     socket.on('all-test-results', this.onAllTestResults.bind(this));
-    socket.on('all-test-results', this.onEnd.bind(runner));
+    socket.on('all-test-results', this.onEnd.bind(this.runner));
 
     var tap = new BrowserTapConsumer(socket);
-    tap.on('test-result', this.onTestResult.bind(runner));
+    tap.on('test-result', this.onTestResult.bind(this.runner));
     tap.on('all-test-results', this.onAllTestResults.bind(this));
   },
 
@@ -120,11 +88,11 @@ BrowserTestRunner.prototype = {
   onTestResult: function(result) {
     var errItems = (result.items || [])
       .filter(function(item) {
-        return !item.passed;
+        return !item.passed || result.failed;
       });
 
     this.reporter.report(this.browserName, {
-      passed: !result.failed && !result.skipped,
+      passed: result.passed && !result.failed && !result.skipped,
       name: result.name,
       skipped: result.skipped,
       runDuration: result.runDuration,
@@ -160,6 +128,40 @@ BrowserTestRunner.prototype = {
 
     this.finish();
   },
+  onGlobalError: function(msg, url, line) {
+    var message = msg + ' at ' + url + ', line ' + line + '\n';
+    this.runner.logs.push({
+      type: 'error',
+      text: message
+    });
+
+    var config = this.launcher.config;
+    if (config.get('bail_on_uncaught_error')) {
+      this.onTestResult.call(this.runner, {
+        passed: false,
+        name: 'Global error: ' + msg + ' at ' + url + ', line ' + line + '\n',
+        logs: [],
+        error: {}
+      });
+      this.onAllTestResults();
+      this.onEnd.call(this.runner);
+    }
+  },
+  onDisconnect: function() {
+    var self = this;
+    this.browser.pending = setTimeout(function() {
+      if (self.finished) {
+        return;
+      }
+
+      var result = {
+        passed: false,
+        name: 'Browser ' + self.launcher.commandLine() + ' disconnected unexpectedly.'
+      };
+      self.onTestResult.call(self.runner, result);
+      self.finish();
+    }, 1000);
+  },
   onProcessExit: function(code) {
     var self = this;
 
@@ -171,7 +173,7 @@ BrowserTestRunner.prototype = {
         passed: false,
         name: 'Browser ' + self.launcher.commandLine() + ' exited unexpectedly with exit code ' + code + '.'
       };
-      self.reporter.report(self.name(), result);
+      self.onTestResult.call(this.runner, result);
       self.finish();
     }, 1000);
   },

tests/runners/browser_test_runner_tests.js
@@ -6,7 +6,7 @@ var EventEmitter = require('events').EventEmitter;
 var Config = require('../../lib/config');
 var Launcher = require('../../lib/launcher.js');
 
-describe('browser test runner', function() {
+describe.only('browser test runner', function() {
   describe('parallel runners', function() {
     var runner, reporter, launcher;
     var ff = {
@@ -39,7 +39,7 @@ describe('browser test runner', function() {
       runner.tryAttach(ff.name, launcher.id, ff.socket);
       runner.tryAttach(chrome.name, launcher.id, chrome.socket);
 
-      ff.socket.emit('test-result', {failed: 1, name: 'Test1'});
+      ff.socket.emit('test-result', {passed: false, name: 'Test1'});
       chrome.socket.emit('test-result', {passed: true, name: 'Test2'});
 
       ff.socket.emit('all-test-results');
@@ -107,6 +107,7 @@ describe('browser test runner', function() {
 
     it('counts a test as passed if it has no failures and has not been skipped', function() {
       runner.onTestResult({
+        passed: true,
         failed: 0,
         skipped: false,
         name: 'passed test',

tests/ci/ci_tests.js
@@ -103,6 +103,64 @@ describe('ci mode app', function() {
         app.start();
       });
     });
+
+    it('returns with non zero exit code when detected a global error', function(done) {
+      var dir = path.join('tests/fixtures/global-error');
+      var config = new Config('ci', {
+        file: path.join(dir, 'testem.json'),
+        port: 0,
+        cwd: dir,
+        launch_in_ci: ['phantomjs'],
+        reporter: new TestReporter(true)
+      });
+      config.read(function() {
+        var app = new App(config, function(exitCode) {
+          expect(exitCode).to.eq(1);
+          done();
+        });
+        app.start();
+      });
+    });
+
+    it('returns with non zero exit code when browser exits', function(done) {
+      var dir = path.join('tests/fixtures/slow-pass');
+      var config = new Config('ci', {
+        file: path.join(dir, 'testem.json'),
+        port: 0,
+        cwd: dir,
+        launch_in_ci: ['phantomjs'],
+        reporter: new TestReporter(true)
+      });
+      config.read(function() {
+        var app = new App(config, function(exitCode) {
+          expect(exitCode).to.eq(1);
+          done();
+        });
+        app.start();
+
+        setTimeout(function() {
+          app.launchers()[0].process.kill();
+        }, 2000);
+      });
+    });
+
+    it('returns with non zero exit code when browser disconnects', function(done) {
+      var dir = path.join('tests/fixtures/disconnect-test');
+      var config = new Config('ci', {
+        file: path.join(dir, 'testem.json'),
+        port: 0,
+        cwd: dir,
+        launch_in_ci: ['phantomjs'],
+        reporter: new TestReporter(true)
+      });
+      config.read(function() {
+        var app = new App(config, function(exitCode) {
+          expect(exitCode).to.eq(1);
+          done();
+        });
+        app.start();
+      });
+    });
   });
 
   it('fails with explicitly defined missing launchers', function(done) {

tests/fixtures/disconnect-test/tests.js
@@ -1,0 +1,11 @@
+/* globals QUnit, window */
+QUnit.test('says hello world', function(assert) {
+  var done = assert.async();
+  setTimeout(function() {
+    done();
+  }, 30000);
+
+  setTimeout(function() {
+    window.location = 'http://google.de';
+  }, 500);
+});

tests/fixtures/global-error/error.js
@@ -1,0 +1,1 @@
+throw new Error('global error');

tests/fixtures/global-error/test.html
@@ -1,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<title>Test'em</title>
+<script src="/testem.js"></script>
+<script src="//code.jquery.com/qunit/qunit-1.20.0.js"></script>
+<script>
+QUnit.config.autostart = false;
+setTimeout(function() {
+  QUnit.start();
+}, 250);
+Testem.hookIntoTestFramework();
+</script>
+<script src="error.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/qunit/qunit-1.20.0.css"/>
+</head>
+<body>
+<div id="qunit"></div>
+<div id="qunit-fixture"></div>
+</body>
+</html>

tests/fixtures/slow-pass/tests.js
@@ -1,0 +1,7 @@
+/* globals QUnit */
+QUnit.test('says hello world', function(assert) {
+  var done = assert.async();
+  setTimeout(function() {
+    done();
+  }, 30000);
+});

commit dfec528aca7adc2cf1ce854e43b5eeb2a063669a

Make tests without checks pass again
Tests which report their result with a numerical `passed` status would
fail when this value is falsy, i.e. "0". This is a regression which was
introduced in commit abb0fdc.

Make this work again even if the value is a number and add a test case.

Fixes #749

lib/runners/browser_test_runner.js
@@ -87,7 +87,7 @@ BrowserTestRunner.prototype = {
       });
 
     this.reporter.report(this.browser, {
-      passed: result.passed && !result.failed && !result.skipped,
+      passed: (result.passed !== false) && !result.failed && !result.skipped,
       name: result.name,
       skipped: result.skipped,
       runDuration: result.runDuration,

tests/runners/browser_test_runner_tests.js
@@ -106,6 +106,19 @@ describe('browser test runner', function() {
       expect(reporter.skipped).to.equal(1);
     });
 
+    it('counts a test as passed when no failures occurred', function() {
+      runner.onTestResult({
+        name: 'no checks',
+        items: [],
+        failed: 0,
+        passed: 0,
+        skipped: false,
+        total: 0,
+        runDuration: 20
+      });
+      expect(reporter.pass).to.equal(1);
+    });
+
     it('counts a test as passed if it has no failures and has not been skipped', function() {
       runner.onTestResult({
         passed: true,