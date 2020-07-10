68c5369a697e9bdf78d5c56a144a8e0f3bc72fb2
Giovanni Tirloni <giovanni.tirloni@gmail.com>
2020-07-10 02:34:43 -0300

    Add Microsoft Edge launcher (#1410)
    * Add Edge launcher

--- public/testem/testem_connection.js
+++ public/testem/testem_connection.js
@@ -101,7 +101,9 @@ function getBrowserName(userAgent) {
       return ['Opera', m[2]].join(' ');
     }],
     /(Opera).*Version\/([0-9]+\.[0-9]+)/,
-    /(Edge)\/([0-9]+\.[0-9]+)/,
+    [/(Edg)\/([0-9]+\.[0-9]+)/, function(m) {
+      return ['Edge', m[2]].join(' ');
+    }],
     /(Chrome)\/([0-9]+\.[0-9]+)/,
     /(Firefox)\/([0-9a-z]+\.[0-9a-z]+)/,
     /(PhantomJS)\/([0-9]+\.[0-9]+)/,
