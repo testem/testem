testem/testem

commit a690bfa41708b9ef2903c34cee214ba70862e88d

update to support asynchronous callbacks from the browser
use common API; simplified method; required callback for sync functions

removed unneeded return; rename var

readme

update coverage example

fix lint error

fix tap runner

lib/runners/browser_test_runner.js
@@ -94,10 +94,12 @@ BrowserTestRunner.prototype = {
 
     socket.on('all-test-results', this.onAllTestResults.bind(this));
     socket.on('all-test-results', this.onEnd.bind(this));
+    socket.on('after-tests-complete', this.onAfterTests.bind(this));
 
     var tap = new BrowserTapConsumer(socket);
     tap.on('test-result', this.onTestResult.bind(this));
     tap.on('all-test-results', this.onAllTestResults.bind(this));
+    tap.on('after-tests-complete', this.onAfterTests.bind(this));
   },
 
   name: function() {
@@ -144,7 +146,8 @@ BrowserTestRunner.prototype = {
   },
   onAllTestResults: function() {
     log.info('Browser ' + this.name() + ' finished all tests.', this.singleRun);
-
+  },
+  onAfterTests: function() {
     this.finish();
   },
   onGlobalError: function(msg, url, line) {

examples/coverage_istanbul/tests.html
@@ -8,12 +8,18 @@
 <script src="/node_modules/jasmine-core/lib/jasmine-core/boot.js"></script>
 <script src="/testem.js"></script>
 <script>
-  Testem.on('all-test-results', function(results) {
-    var xhr = new XMLHttpRequest();
-    var coverage = JSON.stringify(window.__coverage__);
-    xhr.open('POST', '/coverage');
-    xhr.send(coverage);
-  });
+  Testem.afterTests(
+    function(config, data, callback) {
+      var coverage = JSON.stringify(window.__coverage__);
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+          callback();
+        }
+      };
+      xhr.open('POST', '/coverage', true);
+      xhr.send(coverage);
+    });
 </script>
 <script src="/instrumented/hello.js"></script>
 <script src="/instrumented/hello_tests.js"></script>

lib/runners/tap_process_test_runner.js
@@ -16,7 +16,7 @@ TapProcessTestRunner.prototype = {
 
     this.tapConsumer = new TapConsumer();
     this.tapConsumer.on('test-result', this.onTestResult.bind(this));
-    this.tapConsumer.on('all-test-results', this.onAllTestResults.bind(this));
+    this.tapConsumer.on('after-tests-complete', this.onAllTestResults.bind(this));
 
     this.launcher.once('processError', this.onProcessError.bind(this));
     this.launcher.start();

lib/tap_consumer.js
@@ -73,11 +73,11 @@ TapConsumer.prototype = {
     };
 
     this.emit('test-result', test);
-    this.emit('all-test-results');
+    this.emit('after-tests-complete');
   },
   onTapEnd: function() {
     this.stream.removeAllListeners();
-    this.emit('all-test-results');
+    this.emit('after-tests-complete');
   }
 };
 

public/testem/testem_client.js
@@ -100,6 +100,7 @@ function init() {
     }
   };
   hookIntoTestFramework(Testem);
+  Testem.on('all-test-results', Testem.runAfterTests);
 }
 
 function setupTestStats() {
@@ -180,6 +181,7 @@ window.Testem = {
   // set during init
   initTestFrameworkHooks: undefined,
   emitMessageQueue: [],
+  afterTestsQueue: [],
   useCustomAdapter: function(adapter) {
     adapter(this);
   },
@@ -295,6 +297,23 @@ window.Testem = {
     // decycle to remove possible cyclic references
     // stringify for clients that only can handle string postMessages (IE <= 10)
     return JSON.stringify(decycle(message));
+  },
+  runAfterTests: function() {
+    if (Testem.afterTestsQueue.length) {
+      var afterTestsCallback = this.afterTestsQueue.shift();
+
+      if (typeof afterTestsCallback !== 'function') {
+        throw Error('Callback not a function');
+      } else {
+        afterTestsCallback.call(this, null, null, Testem.runAfterTests);
+      }
+
+    } else {
+      emit('after-tests-complete');
+    }
+  },
+  afterTests: function(cb) {
+    Testem.afterTestsQueue.push(cb);
   }
 };
 

commit b74cc359ae525bf6f23bc0e03b860b518ed60dd4

Fix bail_on_uncaught_error
`bail_on_uncaught_error` currently does not cause testem to shut down after a top-level error.  So if a top-level error causes your test suite to be dead, testem will hang forever.

It looks like this feature was broken by  https://github.com/testem/testem/commit/a690bfa41708b9ef2903c34cee214ba70862e88d

Prior to that change, it was sufficient for the error handler to call `onAllTestResults()`, because `onAllTestResults` invoked `finish()`.

This PR restores the behavior of calling `finish()` when trying to bail.

lib/runners/browser_test_runner.js
@@ -248,7 +248,7 @@ module.exports = class BrowserTestRunner {
         error: {}
       });
       this.onAllTestResults();
-      this.onEnd.call(this);
+      this.finish();
     }
   }
 