ab3c986f1d334ad2b403d395bc2d70f027875106
Gaurav Munjal <gaurav.munjal@civicplus.com>
2026-09-03 08:33:02 -0400

    fix: node example

---
+++ examples/node_example/.mocharc.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// Keep this example's Mocha config local so Mocha 12 does not walk up to
+// the repository-root .mocharc.js (which requires tests/_prepare.js).
+module.exports = {};
