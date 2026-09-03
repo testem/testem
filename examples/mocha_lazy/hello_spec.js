5cfa1216e6d48d6c5805315cab5713a139f9c954
Gaurav Munjal <gaurav.munjal@civicplus.com>
2026-09-03 11:31:08 -0400

    Upgrade mocha to v12 (#2043)
    * Upgrade mocha to v12
    
    * upgrade examples to mocha 12
    
    * fix: node example

--- examples/mocha_lazy/hello_spec.js
+++ examples/mocha_lazy/hello_spec.js
@@ -1,4 +1,6 @@
-var expect = chai.expect;
+import { expect } from '/node_modules/chai/index.js';
+
+const { describe, it } = globalThis;
 
 describe('hello', function(){
     it('should say hello', function(){
@@ -8,3 +10,5 @@ describe('hello', function(){
         expect(hello('Bob')).to.equal('hello Bob');
     });
 });
+
+mocha.run();
--- examples/mocha_simple/hello_spec.js
+++ examples/mocha_simple/hello_spec.js
@@ -1,8 +1,14 @@
+import { expect } from '/node_modules/chai/index.js';
+
+const { describe, it } = globalThis;
+
 describe('hello', function(){
     it('should say hello', function(){
-        expect(hello()).to.be('hello world');
+        expect(hello()).to.equal('hello world');
     });
     it('should say hello to person', function(){
-        expect(hello('Bob')).to.be('hello Bob');
+        expect(hello('Bob')).to.equal('hello Bob');
     });
 });
+
+mocha.run();
